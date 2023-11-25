53c47ae81fcbee5277c4bf6d66fdb89ee60e7f97
The-Doof <stanriaz@gmail.com>
2023-11-25 00:22:42 +0000

    Update SpawnManager.cs

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //enemy prefab
     public GameObject[] enemyPrefabs;
     private float spawnRangeX = 30;
     private float spawnPosZ = 30;
-    //private float spawnRangeY = 30;
+    private float spawnRangeY = 30;
     private float startDelay = 1.5f;
     private float spawnInterval = 1.5f;
 
@@ -21,16 +22,23 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+
+
     }
 
     void SpawnEnemy() 
     {
-        //Random animal spawn
+        //max number of enemies spawnable on level = 9
+        //spawn 3 enemies at scene start
+        //when number of enemies fall bellow 3, spawn new enemy at random spawnLoc
+        
+
+
+        //SpawnCoount stores random value based on number of enemy variation
         int spawnCount = UnityEngine.Random.Range(0, enemyPrefabs.Length);
 
-        //randomising position spawn
-        Vector3 spawnPos = new Vector3(UnityEngine.Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        //spawn position 
+        Vector3 spawnPos = new Vector3();
 
         //random location spawn positioning
         Instantiate(enemyPrefabs[spawnCount], spawnPos, enemyPrefabs[spawnCount].transform.rotation);
