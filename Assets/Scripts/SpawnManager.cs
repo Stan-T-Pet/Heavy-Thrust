c22eea3c1e385066286306b7b3af2e279dbaa004
The-Doof <stanriaz@gmail.com>
2023-11-26 22:50:54 +0000

    UI

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,89 +1,85 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SpawnManager : MonoBehaviour
 {
-    // Enemy prefabs array
+    //Enemy prefabs array
     public GameObject[] enemyPrefabs;
 
-    // Enemy spawn locations array
+    //Enemy spawn locations array
     public GameObject[] enemySpawnPoints;
 
-    // Spawn interval in seconds
+    //Spawn interval in seconds
     private float spawnInterval = 2.5f;
 
-    // Current spawn count
+    //Current spawn count
     private int spawnCount = 0;
 
-    // Spawn delay in seconds
+    //Spawn delay in seconds
     private int spawnDelay = 2;
 
-    // Maximum number of enemies to spawn
+    //Maximum number of enemies to spawn on level
     private int spawnMax = 5;
 
-    // Current enemy kill count
+    //Current enemy kill count
     private int killCount = 0;
-
-    // Start is called before the first frame update
     void Start()
     {
-        // Invoke a repeating function to spawn enemies at regular intervals
+        //function repeats to spawn enemies at regular intervals
         InvokeRepeating("SpawnEnemy", spawnDelay, spawnInterval);
     }
 
-    // Update is called once per frame
     void Update()
     {
-        // Check if the spawn count is less than the maximum spawn count
+        //Checking if the spawn count is less than max
         if (spawnCount < spawnMax)
         {
-            // Check if the kill count has reached the maximum
+            //Check if the kill count has reached the max
             if (killCount >= spawnMax)
             {
-                // End the round and print a victory message
-                Debug.Log("End of Round - Victory!");
+                //victory message
+                UnityEngine.Debug.Log("End of Round - Victory!");
             }
             else
-            {
-                // Increment the spawn count
+            { 
                 spawnCount++;
 
-                // Spawn an enemy
-                SpawnEnemy();
+                SpawnEnemy();//Spawn an enemy
             }
         }
     }
 
+    //method to spawn an enemies
     void SpawnEnemy()
     {
-        // Select a random enemy prefab
+        //Select random enemy prefab
         int enemyIndex = UnityEngine.Random.Range(0, enemyPrefabs.Length);
         GameObject enemyPrefab = enemyPrefabs[enemyIndex];
 
-        // Get the next enemy spawn point
+        //find nxt enemy spawn point
         int spawnPointIndex = UnityEngine.Random.Range(0, enemySpawnPoints.Length);
         Vector3 spawnPos = enemySpawnPoints[spawnPointIndex].transform.position;
 
-        // Instantiate the enemy prefab at the spawn position
+        //spawn the enemy prefab at the spawn position
         Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
     }
 
-    // Function to be called when an enemy is killed
+    //when an enemy is killed add to kill counter
     public void EnemyKilled()
     {
-        // Increment the kill count
-        killCount++;
-        Debug.Log("Enemies Killed" + killCount);
-
-        // Check if the kill count has reached the maximum
-        if (killCount >= spawnMax)
         
+        killCount++;//add to count
+        UnityEngine.Debug.Log("Enemies Killed: " + killCount);//msg
+
+        // Check if the kill counter is  max
+        if (killCount >= spawnMax)
         {
-                Debug.Log("Victory");
-                SceneManager.LoadScene("Victory");//End of round Load next Scene
+            UnityEngine.Debug.Log("Victory");
+            SceneManager.LoadScene("Victory"); // End of round, load the next scene
         }
     }
 }
