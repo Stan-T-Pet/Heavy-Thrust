7dcc75f8034bd34651bf474dcc5e95956ca811d0
b00144312 <114091004+b00144312@users.noreply.github.com>
2023-11-26 02:37:32 +0000

    ok buddy
    sd

---
+++ Assets/Scripts/PlayerBullet.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBullet : MonoBehaviour
+{
+    public float speed = 50f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Initialization if needed
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the bullet forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+    }
+
+}
